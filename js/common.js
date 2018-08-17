c1274795b5e8e0b68172963f66caf787d40521ca
leson <234251853@qq.com>
2018-08-17 15:57:44 +0800

    初始化修改,设置媒体查询,>500 是 font-size 写死50px

--- js/common.js
+++ js/common.js
@@ -1,6 +1,8 @@
  //使用媒体查询,设置rem大小
  document.querySelector('html').style.fontSize = window.screen.width/10 +'px';
-
+ if(window.screen.width >= 500){
+    document.querySelector('html').style.fontSize = "50px";
+ }
 
 
 //入口函数
