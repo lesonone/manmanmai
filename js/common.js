89649d1959be6066ba0d06956d64f3f00dd38bfc
leson <234251853@qq.com>
2018-08-17 15:17:46 +0800

    初始化完成

---
+++ js/common.js
@@ -0,0 +1,12 @@
+ //使用媒体查询,设置rem大小
+ document.querySelector('html').style.fontSize = window.screen.width/10 +'px';
+
+
+
+//入口函数
+$(function(){
+    //初始化内部滚动插件
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005
+    });
+})
