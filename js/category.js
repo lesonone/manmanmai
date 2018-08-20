50dab366c637aca7b7328c6d8c1beb948dcd909a
leson <234251853@qq.com>
2018-08-20 17:52:05 +0800

    更新

--- js/category.js
+++ js/category.js
@@ -30,14 +30,6 @@ $(function () {
                     var html = template("category", res);
                     $(".hate").html(html);
                     $(".hate").on("tap",".subMenu",function(){
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-
-=======
->>>>>>> 94b1ee5510b121a687bde63c83e81550cb935f48
->>>>>>> 3c622fa59d231c766e221bdbde01b3aab6475ab0
                         location.href="./subCategory.html?categoryid="+$(this).data("id");
                 
                     })
