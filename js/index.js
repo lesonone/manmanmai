b88be019b643ec838cf22f09cbc981e5ff2d1cf0
yaoyuanzhen <2810720995@qq.com>
2018-08-20 09:41:26 +0800

    完成首页

---
+++ js/index.js
@@ -0,0 +1,36 @@
+$(function(){
+    //获取导航菜单数据;
+    $.ajax({
+        url:"http://mmb.ittun.com/api/getindexmenu",
+        type:"GET",
+        success:function(res){
+            console.log(res);
+            var htmlNav = template("navlist",res);
+            $('.nav').html(htmlNav);
+        }
+
+    });
+
+    //获取超值折扣
+    $.ajax({
+        url:"http://mmb.ittun.com/api/getmoneyctrl",
+        type:"GET",
+        success:function(res){
+            console.log(res);
+
+            var htmlPro = template("productlist",res);
+            $(".products").html(htmlPro);
+        }
+    });
+
+    //点击按钮跳转到省钱控页面
+    $("button").click(function(){
+        window.location.href = "./moneyctrl.html"
+    });
+
+
+    //导航点击更多显示下面四个菜单
+    $('.nav').on("click","li:nth-child(8)",function(){
+        $('.nav li:nth-child(n+9)').toggle();
+    })
+})
