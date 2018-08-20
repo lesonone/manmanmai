bb131de029c4a01220dfe5dd2151d765d3a447ad
handy <1763223463@qq.com>
2018-08-20 15:42:03 +0800

    更改

--- js/category.js
+++ js/category.js
@@ -30,11 +30,7 @@ $(function () {
                     var html = template("category", res);
                     $(".hate").html(html);
                     $(".hate").on("tap",".subMenu",function(){
-<<<<<<< HEAD
-                        console.log(1);
-=======
        
->>>>>>> 7a270ce28770843b1b3b8d9913338025f53f50e6
                         location.href="./subCategory.html?categoryid="+$(this).data("id");
                 
                     })
